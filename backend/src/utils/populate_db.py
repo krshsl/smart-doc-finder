b7f84aaf677536d8d86ae7466259e64cffcd20b8
Krishna Sathyamurthy <64264567+krshsl@users.noreply.github.com>
2025-08-11 03:12:01 -0400

    disabling create_guest_data

--- backend/src/utils/populate_db.py
+++ backend/src/utils/populate_db.py
@@ -1,9 +1,6 @@
-from asyncio import create_task
-
-from .db_oprs.init_data import create_guest_data
 from .db_oprs.init_users import create_initial_users
 
 
 async def populate_db(app_vars):
     await create_initial_users(app_vars)
-    create_task(create_guest_data())
+    # create_task(create_guest_data())
