75792bf5773d793ed250657c6ede9dc35e3578fd
Krishna Sathyamurthy <64264567+krshsl@users.noreply.github.com>
2025-08-15 02:18:08 -0400

    remove aws self signed certs and admin for security
    1. self signed certs need a domain, and i am too broke for it. 2. admin
    password can't be sent in http anymore.

--- backend/src/utils/db_oprs/init_users.py
+++ backend/src/utils/db_oprs/init_users.py
@@ -4,17 +4,7 @@ import src.utils.auth as auth
 async def create_initial_users(app_vars):
     from src.models.user import User
 
-    admin_exists = await User.find_one(User.username == "admin")
     guest_exists = await User.find_one(User.username == "guest")
-
-    if not admin_exists:
-        admin = User(
-            email="admin@example.com",
-            username="admin",
-            password=auth._get_password_hash(app_vars.DB_PASS),
-            role="admin",
-        )
-        await admin.insert()
 
     if not guest_exists:
         guest = User(
