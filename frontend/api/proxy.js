2b224db83297db1214df41ee738ce51b8c9563ae
Krishna Sathyamurthy <64264567+krshsl@users.noreply.github.com>
2025-08-16 14:38:43 -0400

    fix proxy and logout errors

--- frontend/api/proxy.js
+++ frontend/api/proxy.js
@@ -1,8 +1,13 @@
+import { createProxyMiddleware } from "http-proxy-middleware";
+
 export default function handler(req, res) {
   const target = process.env.BACKEND_URL;
 
   return createProxyMiddleware({
     target,
     changeOrigin: true,
+    pathRewrite: {
+      "^/api": "",
+    },
   })(req, res);
 }
